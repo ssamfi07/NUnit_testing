a226ce6a6b82b7390b3f835587dda81868c28abb
ssamfi07 <simsafir@gmail.com>
2021-12-04 18:30:17 +0200

    Support

---
+++ TestDoubles/InternalAccountSpy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank
+{
+    //overrides Deposit & Withdraw methods of the Account class in order to record the method calls. 
+    //this is an Internal Spy, not a Dependency Spy, because the SUT call siblings methods of the same class not external classes/methods.
+    public class InternalAccountSpy : Account
+    {
+        List<String> actions = new List<string>();
+        public new void Deposit(float amount)
+        {
+            base.Deposit(amount);
+            actions.Add("Deposit "+amount);
+        }
+
+        public new void Withdraw(float amount)
+        {
+            base.Withdraw(amount);
+            actions.Add("Withdraw " + amount);
+        }
+
+        public List<String> GetActions()
+        {
+            return actions;
+        }
+    }
+}
---
+++ TestDoubles/LogSpy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace bank
+{
+    public class LogSpy: ILogger
+    {
+        List<String> actions = new List<string>();
+        public void Log(String message)
+        {
+            actions.Add(MethodBase.GetCurrentMethod().Name + "(" + DateTime.Now + ") "+ message);
+        }
+
+
+        public List<String> GetActions()
+        {
+            return actions;
+        }
+
+        public int GetNumberOfCalls()
+        {
+            return actions.Count;
+        }
+    }
+}
