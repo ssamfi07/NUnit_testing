90c4fd7c9384d1b03906ce80e021538c0ba83d78
Samfirescu Stefan <simsafir@gmail.com>
2021-12-06 21:38:31 +0200

    Code improvement

--- Account.Tests/AccoutTestMockFramework.cs
+++ Account.Tests/AccoutTestMockFramework.cs
@@ -31,9 +31,9 @@ public class AccountTestMockFramework
         var source = new AccountForLogSpy(200, client, logMock.Object);
         var destination = new AccountForLogSpy(150, client, logMock.Object);
 
-        //set mocked logger expectations
+        // set mocked logger expectations
         logMock.Setup(d => d.Log("method Log was called with message : Transaction : 100 & 250"));
-        // logMock.ExpectedNumberOfCalls(1);
+        // logMock.ExpectedNumberOfCalls(1); -- private method, cannot be called
 
         // act
         source.TransferFunds2(destination, 50.00F);
@@ -42,7 +42,7 @@ public class AccountTestMockFramework
         Assert.AreEqual(200.00F, destination.Balance);
         Assert.AreEqual(150.00F, source.Balance);
 
-        //mock object verify
+        // mock object verify
         logMock.Verify();
     }
 
@@ -50,21 +50,21 @@ public class AccountTestMockFramework
     public void TransferFundsFromEurAmount_MockFramework_ShouldWork()
     {
 
-        //arrange
+        // arrange
         var source = new Account(200);
         var destination = new Account(150);
         var rateEurRon = 4.95F;
         var convertorMock = new Mock<ICurrencyConvertor>();
-        convertorMock.Setup(_ => _.EurToRon(20)).Returns(20*rateEurRon); // set mock to act as a TestDouble Stub - gives IndirectInputs to the SUT
+        convertorMock.Setup(_ => _.EurToRon(20)).Returns(20*rateEurRon);  // set mock to act as a TestDouble Stub - gives IndirectInputs to the SUT
 
         // act
         source.TransferFundsFromEurAccount_version2(destination, 20.00F, convertorMock.Object);
 
-        //assert
+        // assert
         Assert.AreEqual(150.00F + 20 * rateEurRon, destination.Balance);
         Assert.AreEqual(200.00F - 20 * rateEurRon, source.Balance);
 
-        convertorMock.Verify(_ => _.EurToRon(20), Times.Once()); // verify behavior 
+        convertorMock.Verify(_ => _.EurToRon(20), Times.Once());  // verify behavior 
     }
 }
 
